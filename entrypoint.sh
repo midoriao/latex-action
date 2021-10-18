2e3dc84b61c2e3b3570123a2313fde714a50870b
Sota Sato <sotasato@nii.ac.jp>
2021-10-18 12:59:16 +0900

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ ARGS="-file-line-error -halt-on-error -interaction=nonstopmode"
 WORKING_DIR="$1"
 LATEX_SOURCE="$2"
 
+IFS=' ' read -r -a ARGS <<< "$ARGS"
+
 cd "$WORKING_DIR"
 
-"$COMPILER" "$ARGS" "$LATEX_SOURCE"
+"$COMPILER" "${ARGS[@]}" "$LATEX_SOURCE"
